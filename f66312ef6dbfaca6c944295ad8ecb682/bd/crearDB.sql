09fea97831016fdf87cd1539e99110183087df06
Enery923 <isuarg00@estudiantes.unileon.es>
2016-06-13 09:24:39 +0200

    cambio del nombre de los ficheros

---
+++ f66312ef6dbfaca6c944295ad8ecb682/bd/crearDB.sql
@@ -0,0 +1,7 @@
+
+drop database if exists 'f66312ef6dbfaca6c944295ad8ecb682';
+
+create database 'f66312ef6dbfaca6c944295ad8ecb682';
+
+grant all on subastaDB.* to  'f66312ef6dbfaca6'@localhost identified by 'sekret';
+
