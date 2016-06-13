5a0c9ae22a2c0ddad17ce9af96d26890a5020408
David <descug01@estudiantes.unileon.es>
2016-06-13 10:05:42 +0200

    cambiado nombres bases de datos

--- f10ff783408cc881860e701a270e4387/database.php
+++ f10ff783408cc881860e701a270e4387/database.php
@@ -1,6 +1,6 @@
 <?php
-	$database = 'mysql:host=localhost;dbname=subastaDB;charset=utf8';
-	$userDatabase = 'mysql';
+	$database = 'mysql:host=localhost;dbname=f10ff783408cc881860e701a270e4387;charset=utf8';
+	$userDatabase = 'f10ff783408cc881';
 	$passwordDatabase = 'sekret';
 	try {
 		$dbh = new PDO($database, $userDatabase, $passwordDatabase);
