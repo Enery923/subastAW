5fde7b021163b024b75d4f6b77549fdd8d27c608
Enery923 <isuarg00@estudiantes.unileon.es>
2016-06-13 09:44:22 +0200

    Correcion nombres de ficheros

---
+++ f10ff783408cc881860e701a270e4387/bd/crearDB.sql
@@ -0,0 +1,7 @@
+
+drop database if exists 'f10ff783408cc881860e701a270e4387';
+
+create database 'f10ff783408cc881860e701a270e4387';
+
+grant all on subastaDB.* to  'f10ff783408cc881'@localhost identified by 'sekret';
+
--- f66312ef6dbfaca6c944295ad8ecb682/bd/crearDB.sql
+++
@@ -1,7 +0,0 @@
-
-drop database if exists 'f66312ef6dbfaca6c944295ad8ecb682';
-
-create database 'f66312ef6dbfaca6c944295ad8ecb682';
-
-grant all on subastaDB.* to  'f66312ef6dbfaca6'@localhost identified by 'sekret';
-
